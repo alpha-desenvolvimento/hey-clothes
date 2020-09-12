75f2659df03d1362fdfcaab815c120ad6008fbab
Gabriel Loch <gabriel.roetger@globant.com>
2020-09-12 15:45:45 -0300

    updated JWT

--- jwt.js
+++ jwt.js
@@ -1,31 +1,26 @@
 require("dotenv-safe").config();
 const jwt = require("jsonwebtoken");
 
+const expiresIn = 60 * 30; // expires in 30 min
+
 async function authUser(args = { user: null, pwd: null }) {
   const { user, pwd } = args;
 
   const resp = {
-    error: true,
-    message: null,
+    error: null,
     auth: false,
     token: null,
   };
 
   if (!user) {
-    resp.message = "Must set a user to authenticate. ";
+    resp.error = "Must set a user to authenticate. ";
   } else if (!pwd) {
-    resp.message = "Must set a password to authenticate. ";
+    resp.error = "Must set a password to authenticate. ";
   } else {
-    resp.error = false;
     resp.auth = user == "e@mail.com" && pwd == "123456";
-
-    const id = 1;
     if (resp.auth) {
-      resp.token = jwt.sign({ id }, process.env.SECRET, {
-        expiresIn: 60 * 30, // expires in 30 min
-      });
-    } else {
-      resp.token = null;
+      const userInfo = { user, pwd, id: 1 };
+      resp.token = jwt.sign(userInfo, process.env.SECRET, { expiresIn });
     }
   }
 
