57c794b98da76767dc34021d684358fd97234649
Gabriel Loch <grl.loch@gmail.com>
2020-09-19 18:48:01 -0300

    Models update

---
+++ helper/requestUtils.js
@@ -0,0 +1,15 @@
+function getRequestParams(req, paramList) {
+  if (typeof paramList == "string") {
+    return req.body[paramList] || req.headers[`x-access-${paramList}`];
+  } else if (Array.isArray(paramList)) {
+    const values = {};
+
+    for (const value of paramList)
+      values[value] = req.body[value] || req.headers[`x-access-${value}`];
+
+    return values;
+  }
+  return null;
+}
+
+module.exports = { getRequestParams };
