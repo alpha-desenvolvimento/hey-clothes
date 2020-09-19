b45a6755f7cec185e14fc73f522864e34558b910
Gabriel Loch <grl.loch@gmail.com>
2020-09-19 02:38:07 -0300

    Added route productProvider on server

--- server.js
+++ server.js
@@ -1,96 +1,34 @@
-
-// console.clear();
-
-const express = require("express"),
-  { json } = require("express"),
+console.clear();
+const { exec } = require("child_process"),
+  express = require("express"),
+  { json, response } = require("express"),
   path = require("path"),
   jwt = require("./jwt"),
   bodyParser = require("body-parser"),
   ServiceResponse = require("./classes/ServiceResponse"),
   cors = require("cors");
+
 require("dotenv-safe").config();
 
-const app = express();
-let router = require("./routes/routes")
-const port = process.env.PORT || 5000;
-const { database } = require("./database/models");
+require("./database/generate")();
 
-const routes = require("./routes");
+const app = express();
+const port = process.env.PORT || 5000;
+// let router = require("./routes/routes");
+// const { models } = require("./database/models");
+
+// const routes = require("./routes");
 
 app.use(cors({ allowedHeaders: "*" }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/",router);
-
-
-//todo api produto create /id
-    //todo api provider
-    //todo api category
-//todo api usuario create
- 
-
-//todo update usuario
-
-//todo delete produto /
-
-
-
-app.all("/api", (req, res) => {
-  const response = new ServiceResponse("Unknow API Name");
-
-  response.setError("Must set a API, ex: /api/<API name>");
-
-  return res.json(response);
-});
-
-app.all("/api/testServer", async (req, res) => {
-  console.log(
-    await database.Product.findAll({
-      include: [
-        {
-          model: database.ProductCategory,
-          required: true,
-        }
-      ],
-    })
-  );
-
-  // try {
-  //   database.User.create({ name: 'Tester 1', email: 'e@mail.com', pwd: '123456' })
-  // } catch (error) { }
-});
-
-app.post("/api/auth/:action", async (req, res) => {
-  const { action } = req.params;
-  var response = new ServiceResponse(
-    "Unknow API Name",
-    "Inform a right API path"
-  );
-
-  switch (action) {
-    case "user":
-      response = await routes.auth.user(req);
-      break;
-    case "token":
-      response = await routes.auth.token(req);
-      break;
-  }
-
-  return res.json(response);
-});
-
-app.all("/api/*", (req, res) => {
-  const response = new ServiceResponse("Unknow API Name");
-
-  response.setError("This API doesn't exist.");
-
-  return res.json(response);
-});
+app.use("/api/provider", require("./routes/productProvider"));
 
 app.use("/*", (req, res) => {
-  return res.sendFile("./index.html");
+  return res.send("?");
+  //   return res.sendFile("index.html");
 });
 
 app.listen(port, () => {
@@ -101,3 +39,67 @@ app.listen(port, () => {
   }
 });
 
+// app.use("/", router);
+
+//todo api produto create /id
+//todo api provider
+//todo api category
+//todo api usuario create
+
+//todo update usuario
+
+//todo delete produto /
+
+// app.all("/api", (req, res) => {
+//   const response = new ServiceResponse("Unknow API Name");
+
+//   response.setError("Must set a API, ex: /api/<API name>");
+
+//   return res.json(response);
+// });
+
+// app.all("/api/testServer", async (req, res) => {
+//   console.log(
+//     await database.Product.findAll({
+//       include: [
+//         {
+//           model: database.ProductCategory,
+//           required: true,
+//         },
+//       ],
+//     })
+//   );
+
+//   // try {
+//   //   database.User.create({ name: 'Tester 1', email: 'e@mail.com', pwd: '123456' })
+//   // } catch (error) { }
+// });
+
+// app.post("/api/auth/:action", async (req, res) => {
+//   const { action } = req.params;
+//   var response = new ServiceResponse(
+//     "Unknow API Name",
+//     "Inform a right API path"
+//   );
+
+//   switch (action) {
+//     case "user":
+//       response = await routes.auth.user(req);
+//       break;
+//     case "token":
+//       response = await routes.auth.token(req);
+//       break;
+//   }
+
+//   return res.json(response);
+// });
+
+// app.all("/api/*", (req, res) => {
+//   const response = new ServiceResponse("Unknow API Name");
+
+//   response.setError("This API doesn't exist.");
+
+//   return res.json(response);
+
+//   res.data.data;
+// });
