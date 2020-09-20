f3b2300ce1b403c0a3eaf2e862bfd843649360e8
Gabriel Loch <grl.loch@gmail.com>
2020-09-20 14:28:45 -0300

    Bug fix on product creating

--- database/models/User.js
+++ database/models/User.js
@@ -4,6 +4,7 @@ const sequelize = require("../connection");
 const Product = require("./Product");
 
 const User = sequelize.define("user", {
+  // id: { type: DataTypes.INTEGER, primaryKey: true },
   name: DataTypes.STRING, //TODO atualizar username para name no documento
   email: {
     type: DataTypes.STRING,
@@ -39,7 +40,5 @@ const User = sequelize.define("user", {
     },
   },
 });
-
-User.hasMany(Product, { as: "createdProducts" });
 
 module.exports = User;
