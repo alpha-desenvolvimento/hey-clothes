90e44fbba367d913df6888fa24c48f8db6b82187
Gabriel Loch <grl.loch@gmail.com>
2020-09-20 15:48:29 -0300

    Bug fix with dotenv-safe v3

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
-// require("dotenv-safe").config();
-require("dotenv-safe").config();
+if (process.env.NODE_ENV !== "production") require("dotenv-safe").config();
 
 module.exports = {
   use_env_variable:"DATABASE_URL",
